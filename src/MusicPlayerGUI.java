6788d3fe17452208baf281c5cb36f03ea30a2fc2
litdebbie <sarah.nk.ngo@gmail.com>
2025-07-31 23:29:51 -0500

    Added toolbar for music player

--- src/MusicPlayerGUI.java
+++ src/MusicPlayerGUI.java
@@ -1,6 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MusicPlayerGUI extends JFrame {
+    // color configurations
+    public static final Color FRAME_COLOR = Color.BLACK;
+    public static final Color TEXT_COLOR = Color.WHITE;
+
     public MusicPlayerGUI() {
         // set title of JFrame to "Music Player"
         super("Music Player");
@@ -20,5 +25,48 @@ public class MusicPlayerGUI extends JFrame {
         // set layout to null allows us to control the (x,y) coordinates of
         // our components and also set the height and width
         setLayout(null);
+
+        // change the frame color
+        getContentPane().setBackground(FRAME_COLOR);
+
+        addGuiComponents();
+    }
+
+    private void addGuiComponents() {
+        // add toolbar
+        addToolbar();
+    }
+
+    private void addToolbar() {
+        JToolBar toolBar = new JToolBar();
+        toolBar.setBounds(0, 0, getWidth(), 25);
+
+        // prevent toolbar from being moved
+        toolBar.setFloatable(false);
+
+        // add drop down menu
+        JMenuBar menuBar = new JMenuBar();
+        toolBar.add(menuBar);
+
+        // add a song menu where we will place the loading song option
+        JMenu songMenu = new JMenu("Song");
+        menuBar.add(songMenu);
+
+        // add the "load song" item in the songMenu
+        JMenuItem loadSong = new JMenuItem("Load Song");
+        songMenu.add(loadSong);
+
+        // add a playlist menu
+        JMenu playlistMenu = new JMenu("Playlist");
+        menuBar.add(playlistMenu);
+
+        // add the items to the playlist menu
+        JMenuItem createPlaylist = new JMenuItem("Create Playlist");
+        playlistMenu.add(createPlaylist);
+
+        JMenuItem loadPlaylist = new JMenuItem("Load Playlist");
+        playlistMenu.add(loadPlaylist);
+
+        add(toolBar);
     }
 }
